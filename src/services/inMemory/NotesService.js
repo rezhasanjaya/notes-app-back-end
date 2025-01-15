9f5af69ab714f681cf9911d7ed83eb5aaa8421e9
rezhasanjaya04 <rezhasanjaya04@gmail.com>
2025-01-15 16:22:23 +0700

    update: module validasi

--- src/services/inMemory/NotesService.js
+++ src/services/inMemory/NotesService.js
@@ -1,4 +1,4 @@
-const { nanoid } = require('nanoid');
+const { nanoid } = require("nanoid");
 
 class NotesService {
   constructor() {
@@ -9,20 +9,24 @@ class NotesService {
     const id = nanoid(16);
     const createdAt = new Date().toISOString();
     const updatedAt = createdAt;
- 
+
     const newNote = {
-      title, tags, body, id, createdAt, updatedAt,
+      title,
+      tags,
+      body,
+      id,
+      createdAt,
+      updatedAt,
     };
 
     this._notes.push(newNote);
 
     const isSuccess = this._notes.filter((note) => note.id === id).length > 0;
     if (!isSuccess) {
-      throw new Error('Catatan gagal ditambahkan');
+      throw new Error("Catatan gagal ditambahkan");
     }
- 
-    return id;
 
+    return id;
   }
 
   getNotes() {
@@ -32,20 +36,20 @@ class NotesService {
   getNoteById(id) {
     const note = this._notes.filter((n) => n.id === id)[0];
     if (!note) {
-      throw new Error('Catatan tidak ditemukan');
+      throw new Error("Catatan tidak ditemukan");
     }
     return note;
   }
 
   editNoteById(id, { title, body, tags }) {
     const index = this._notes.findIndex((note) => note.id === id);
- 
+
     if (index === -1) {
-      throw new Error('Gagal memperbarui catatan. Id tidak ditemukan');
+      throw new Error("Gagal memperbarui catatan. Id tidak ditemukan");
     }
- 
+
     const updatedAt = new Date().toISOString();
- 
+
     this._notes[index] = {
       ...this._notes[index],
       title,
@@ -58,7 +62,7 @@ class NotesService {
   deleteNoteById(id) {
     const index = this._notes.findIndex((note) => note.id === id);
     if (index === -1) {
-      throw new Error('Catatan gagal dihapus. Id tidak ditemukan');
+      throw new Error("Catatan gagal dihapus. Id tidak ditemukan");
     }
     this._notes.splice(index, 1);
   }
