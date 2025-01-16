59ee77fca2a776189a5fdb014fb29878add6cda1
rezhasanjaya04 <rezhasanjaya04@gmail.com>
2025-01-16 11:41:29 +0700

    reconstruct validation method

--- src/server.js
+++ src/server.js
@@ -23,6 +23,23 @@ const init = async () => {
     },
   });
 
+  server.ext("onPreResponse", (request, h) => {
+    // mendapatkan konteks response dari request
+    const { response } = request;
+
+    // penanganan client error secara internal.
+    if (response instanceof ClientError) {
+      const newResponse = h.response({
+        status: "fail",
+        message: response.message,
+      });
+      newResponse.code(response.statusCode);
+      return newResponse;
+    }
+
+    return h.continue;
+  });
+
   await server.start();
   console.log(`Server berjalan pada ${server.info.uri}`);
 };
