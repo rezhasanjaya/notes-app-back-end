a12c6b3ba2a1ee422669ce5e683f64185d0cec62
rezhasanjaya04 <rezhasanjaya04@gmail.com>
2025-03-05 14:43:41 +0700

    pembuatan cache service

---
+++ src/services/redis/CacheService.js
@@ -0,0 +1,16 @@
+const redis = require('redis');
+
+class CacheService {
+  constructor() {
+    this._client = redis.createClient({
+      socket: {
+        host: process.env.REDIS_SERVER,
+      },
+    });
+    this._client.on('error', (error) => {
+      console.error(error);
+    });
+    this._client.connect();
+  }
+}
+
